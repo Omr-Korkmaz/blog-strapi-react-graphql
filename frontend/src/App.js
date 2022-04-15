fcabb6ce9f82e6e13294ebe1942d344bb23755f5
omer Korkmaz <h.omr.korkmaz@gmail.com>
2022-04-16 00:23:22 +0200

    added pages and and react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Details from './pages/Details';
+import Category from './pages/Category';
+import SiteHeader from './components/SiteHeader';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<Router>
+  <SiteHeader />
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/Details" element={<Details/>}/>
+          <Route exact path="/Category" element={<Category/>}/>
+          {/* <Route path="*" element={<NotFound/>}/> */}
+        </Routes>
+    </Router>
+
+
+
+
+
+
+
+
   );
 }
 
---
+++ frontend/src/components/SiteHeader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+const SiteHeader = () => {
+  return (
+    <div>
+        <Link to='/' > <h1>Reviews</h1></Link>
+
+
+
+    </div>
+  )
+}
+
+export default SiteHeader
