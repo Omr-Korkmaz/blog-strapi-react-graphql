0a41aa13edaa2b5ddae53d78959a6ae803977960
omer Korkmaz <h.omr.korkmaz@gmail.com>
2022-04-16 14:02:15 +0200

    frontend - added hook and fetch data from strapi endpoint

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import useFetch from '../hooks/useFetch'
+import { Link } from 'react-router-dom'
 
-const Home = () => {
+export default function Homepage() {
+  const {loading, error, data } = useFetch('http://localhost:1337/api/reviews')
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error, something is wrong</p>
+
+  console.log(data.data)
+
   return (
-    <div>Home</div>
+    <div>
+      {data.data.map(review => (
+        <div key={review.id} >
+          
+          <div >{review.attributes.rating}</div>
+          <h2>{review.attributes.title}</h2>
+          
+          <small>console list</small>
+
+          <p>{review.attributes.body.substring(0, 250)}...</p>
+          <Link to={`/details/${review.id}`}>Read more</Link>
+        </div>
+      ))}
+    </div>
   )
 }
-
-export default Home
