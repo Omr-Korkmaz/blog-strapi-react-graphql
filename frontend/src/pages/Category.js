5b1efa5c0327aafd9107d24a6343815f56c66a20
omer Korkmaz <h.omr.korkmaz@gmail.com>
2022-04-17 23:56:11 +0200

    fixed the graphql for 'many to many' logic

--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -1,81 +1,56 @@
-import React from "react";
-import { useQuery, gql } from "@apollo/client";
-import { useParams, Link } from "react-router-dom";
+import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+import { useParams, Link } from 'react-router-dom'
 
-const CATEGORY = gql`
+const CATEGORY= gql`
+query GetCategory($id: ID!) {
+  category(id: $id) {
+    data {
+      attributes{
+        name,
 
-  query GetCategory($id: ID!) {
-    category(id: $id) {
-      data {
-        attributes{
-          name,
-          reviews {
-            data{
-              attributes{
-                title,
-                rating,
-                body    
+        reviews{
+          data{
+            attributes{
+              title,
+              body,
+              rating,
+              categories{
+                data{
+                  attributes{
+                    name
+                  }
+                  id
+                }
               }
-              id  
-        
+
             }
+            id
           }
-
-
         }
-        id
 
-
-        
+      }
      
     }
   }
-`;
+}
+`
+
 
 export default function Category() {
-  const { id } = useParams();
+  const { id } = useParams()
   const { loading, error, data } = useQuery(CATEGORY, {
-    variables: { id: id },
-  });
+    variables: { id: id }
+  })
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
 
-  console.log(data);
+  console.log('categories, This is the ', data)
 
   return (
     <div>
   
-  <h2>{data.category.data.attributes.name} Games</h2>
-{/* {data.category.data.reviews.map((review) => (
-  <div key={review.id} className="review-card">
-    <div className="rating">{review.data.attributes.rating}</div>
-    <h2>{review.data.attributes.title}</h2>
-
-    {review.data.categories.data.map((c) => (
-      <small key={c.id}>{c.attributes.name}</small>
-    ))}
-
-    <p>{review.data.attributes.body.substring(0, 200)}...</p>
-    <Link to={`/details/${review.id}`}>Read more</Link>
-  </div>
-))} */}
     </div>
-  );
+  )
 }
-
-
-{/* <h2>{data.category.data.attributes.name} Games</h2>
-{data.category.data.reviews.map((review) => (
-  <div key={review.id} className="review-card">
-    <div className="rating">{review.data.attributes.rating}</div>
-    <h2>{review.data.attributes.title}</h2>
-
-    {review.data.categories.data.map((c) => (
-      <small key={c.id}>{c.attributes.name}</small>
-    ))}
-
-    <p>{review.data.attributes.body.substring(0, 200)}...</p>
-    <Link to={`/details/${review.id}`}>Read more</Link>
-  </div>
-))} */}
